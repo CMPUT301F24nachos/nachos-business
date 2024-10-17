95659ebd54fe49e02a8c3f43cda35ef022750e44
usr1 <arbrady@ualberta.ca>
2024-10-17 16:21:40 -0600

    added dashboard fragment

---
+++ app/src/main/java/com/example/nachosbusiness/Dashboard.java
@@ -0,0 +1,40 @@
+package com.example.nachosbusiness;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+
+        SwitchCompat notificationSwitch = findViewById(R.id.notification_switch);
+
+        TextView userID = findViewById(R.id.dashboard_user_id);
+
+        Button yourEventsButton = findViewById(R.id.button_your_events);
+        Button browseButton = findViewById(R.id.button_browse);
+        Button facilityButton = findViewById(R.id.button_facility);
+        Button profileButton = findViewById(R.id.button_profile);
+        Button eventUpdatesButton = findViewById(R.id.button_event_updates);
+        Button joinEventsButton = findViewById(R.id.button_join_events);
+
+    }
+}
