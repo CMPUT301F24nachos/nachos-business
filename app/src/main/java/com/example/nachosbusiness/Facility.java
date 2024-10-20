4ad5c7dcf2f921ab98035375612f18c340f5e099
Aaron Brady <112768233+aabrady6@users.noreply.github.com>
2024-10-20 13:52:07 -0600

    Facility Front End (#100)
    * added dashboard layout
    
    * added dashboard fragment
    
    * added button click empty functions
    
    * added dashboard to Android Manifest
    
    * Added facility layout
    
    * added facility activity and class
    
    * updated to facility to fragment

---
+++ app/src/main/java/com/example/nachosbusiness/Facility.java
@@ -0,0 +1,40 @@
+package com.example.nachosbusiness;
+
+public class Facility {
+    private String name;
+    private String location;
+    private String info;
+
+    public Facility() {
+    }
+
+    public Facility(String name, String location, String info) {
+        this.name = name;
+        this.location = location;
+        this.info = info;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+}
