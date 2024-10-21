4ff164ce1b707b918d05e225633e8204b18df790
usr1 <arbrady@ualberta.ca>
2024-10-20 19:28:44 -0600

    javadocs for facility class

--- app/src/main/java/com/example/nachosbusiness/Facility.java
+++ app/src/main/java/com/example/nachosbusiness/Facility.java
@@ -5,35 +5,68 @@ public class Facility {
     private String location;
     private String info;
 
+    /**
+     * Constructor for a facility, no inputs
+     */
     public Facility() {
     }
 
+    /**
+     * Facility constructor
+     * @param name name of the facility
+     * @param location location name
+     * @param info provided description
+     */
     public Facility(String name, String location, String info) {
         this.name = name;
         this.location = location;
         this.info = info;
     }
 
+    /**
+     * getter for facility name
+     * @return name string
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * setter for facility name
+     * @param name name of the facility
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * getter for location
+     * @return location string
+     */
     public String getLocation() {
         return location;
     }
 
+    /**
+     * setter for location
+     * @param location location string
+     */
     public void setLocation(String location) {
         this.location = location;
     }
 
+    /**
+     * getter for facility description
+     * @return facility desciption string
+     */
     public String getInfo() {
         return info;
     }
 
+    /**
+     * set facility description
+     * @param info description of facility
+     */
     public void setInfo(String info) {
         this.info = info;
     }
