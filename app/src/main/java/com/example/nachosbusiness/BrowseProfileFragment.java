ac1fb25ee1ed7d39e3a4008695a3f4edb5fa0035
ruchali1 <ruchali@ualberta.ca>
2024-10-22 15:33:52 -0600

    Profile and Event Skeleton

--- app/src/main/java/com/example/nachosbusiness/BrowseProfileFragment.java
+++ app/src/main/java/com/example/nachosbusiness/BrowseProfileFragment.java
@@ -1,2 +1,49 @@
-package com.example.nachosbusiness;public class BrowseProfileFragment {
+package com.example.nachosbusiness;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class BrowseProfileFragment extends Fragment {
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        return inflater.inflate(R.layout.browse_profile, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ImageButton eventViewButton = view.findViewById(R.id.eventview);
+        eventViewButton.setOnClickListener(v -> {
+            new AlertDialog.Builder(getActivity())
+                    .setTitle("Switch to Browse Events and Event Images")
+                    .setMessage("Do you want to switch to the Event view?")
+                    .setPositiveButton("Switch", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent intent = new Intent(getActivity(), Browse.class);
+                            startActivity(intent);
+                        }
+                    })
+                    .setNegativeButton("Back", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .show();
+        });
+
+    }
 }
