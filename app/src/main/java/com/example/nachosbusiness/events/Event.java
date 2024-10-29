2c60719db9dda86ec1b5ced4c91ed5a55371dbc6
Tim Yoon <tyoon@ualberta.ca>
2024-10-29 17:09:19 -0600

    Events class, ListManager class

--- app/src/main/java/com/example/nachosbusiness/events/Event.java
+++ app/src/main/java/com/example/nachosbusiness/events/Event.java
@@ -2,6 +2,7 @@ package com.example.nachosbusiness.events;
 
 import com.example.nachosbusiness.Facility;
 import com.example.nachosbusiness.users.User;
+import com.google.firebase.Timestamp;
 
 import java.util.Date;
 import java.util.UUID;
@@ -11,10 +12,10 @@ public class Event {
     private String name;
     private User organizer;
 
-    private Date startTime;
-    private Date endTime;
-    private Date startDate;
-    private Date endDate;
+    private Timestamp startTime;
+    private Timestamp endTime;
+    private Timestamp startDate;
+    private Timestamp endDate;
 
     private String description;
     private String qrCode;
@@ -22,31 +23,134 @@ public class Event {
     private Facility facility;
     private ListManager listManager;
 
-
-    public Event(String name, User organizer, Facility facility)
+    /**
+     * Constructor with date/time as Timestamp datatype
+     * @param name
+     * @param organizer
+     * @param facility
+     * @param description
+     * @param startDate
+     * @param endDate
+     * @param startTime
+     * @param endTime
+     */
+    public Event(String name, User organizer, Facility facility, String description, Timestamp startDate, Timestamp endDate, Timestamp startTime, Timestamp endTime)
     {
         this.eventID = UUID.randomUUID().toString();
 
         this.name = name;
         this.organizer = organizer;
         this.facility = facility;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.startTime = startTime;
+        this.endTime = endTime;
 
-        listManager = new ListManager();
+        listManager = new ListManager(eventID);
     }
 
-    public Event(String name, User organizer, Facility facility, int waitListSpots)
+    /**
+     * Constructor with specified number of wait list spots. Date/time as Timestamp datatype
+     * @param name
+     * @param organizer
+     * @param facility
+     * @param description
+     * @param startDate
+     * @param endDate
+     * @param startTime
+     * @param endTime
+     * @param waitListSpots
+     */
+    public Event(String name, User organizer, Facility facility, String description, Timestamp startDate, Timestamp endDate, Timestamp startTime, Timestamp endTime, int waitListSpots)
     {
         this.eventID = UUID.randomUUID().toString();
 
         this.name = name;
         this.organizer = organizer;
         this.facility = facility;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.startTime = startTime;
+        this.endTime = endTime;
 
-        listManager = new ListManager(waitListSpots);
+        listManager = new ListManager(eventID, waitListSpots);
     }
 
+    /**
+     * Constructor with date/time as Date datatype
+     * @param name
+     * @param organizer
+     * @param facility
+     * @param description
+     * @param startDate
+     * @param endDate
+     * @param startTime
+     * @param endTime
+     */
+    public Event(String name, User organizer, Facility facility, String description, Date startDate, Date endDate, Date startTime, Date endTime)
+    {
+        this.eventID = UUID.randomUUID().toString();
 
-    public ListManager getListManager() {
-        return listManager;
+        this.name = name;
+        this.organizer = organizer;
+        this.facility = facility;
+        this.description = description;
+        this.startDate =  new Timestamp(startDate);
+        this.endDate = new Timestamp(endDate);
+        this.startTime = new Timestamp(startTime);
+        this.endTime = new Timestamp(endTime);
+
+        listManager = new ListManager(eventID);
     }
+
+    /**
+     * Constructor with specified number of waitlist spots. Date/time as Date datatype
+     * @param name
+     * @param organizer
+     * @param facility
+     * @param description
+     * @param startDate
+     * @param endDate
+     * @param startTime
+     * @param endTime
+     * @param waitListSpots
+     */
+    public Event(String name, User organizer, Facility facility, String description, Date startDate, Date endDate, Date startTime, Date endTime, int waitListSpots)
+    {
+        this.eventID = UUID.randomUUID().toString();
+
+        this.name = name;
+        this.organizer = organizer;
+        this.facility = facility;
+        this.description = description;
+        this.startDate =  new Timestamp(startDate);
+        this.endDate = new Timestamp(endDate);
+        this.startTime = new Timestamp(startTime);
+        this.endTime = new Timestamp(endTime);
+
+        listManager = new ListManager(eventID, waitListSpots);
+    }
+
+    public User getOrganizer() { return organizer; }
+
+    public void setOrganizer(User organizer) { this.organizer = organizer; }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getEventID() { return eventID; }
+
+    public void setEventID(String eventID) { this.eventID = eventID; }
+
+    public String getDescription() { return description; }
+
+    public void setDescription(String description) { this.description = description; }
+
+    public Facility getFacility() { return facility; }
+
+    public void setFacility(Facility facility) { this.facility = facility; }
+
+    public ListManager getListManager() { return listManager; }
 }
