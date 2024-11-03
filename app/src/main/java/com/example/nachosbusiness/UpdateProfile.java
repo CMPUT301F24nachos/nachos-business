f6cec413baca2199d2cb3682b4d5e1efd135c14f
nopunindented <umerfiaz251@gmail.com>
2024-11-02 22:26:07 -0600

    changes

--- app/src/main/java/com/example/nachosbusiness/UpdateProfile.java
+++ app/src/main/java/com/example/nachosbusiness/UpdateProfile.java
@@ -1,39 +1,102 @@
 package com.example.nachosbusiness;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.telephony.PhoneNumberUtils;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.Toast;
-import androidx.appcompat.app.AppCompatActivity;
 
-public class UpdateProfile extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.nachosbusiness.users.RegistrationActivity;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+public class UpdateProfile extends Fragment {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_update);
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        return inflater.inflate(R.layout.activity_update_profile_final, container, false);
+    }
 
-        TextView userName = findViewById(R.id.user_name);
-        TextView email = findViewById(R.id.user_email);
-        TextView phoneNumber = findViewById(R.id.user_phone);
-        RelativeLayout updateProfileButton = findViewById(R.id.update_profile_button);
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-        updateProfileButton.setOnClickListener(new View.OnClickListener() {
+        Button saveButton = view.findViewById(R.id.signUpButton);
+        Button cancelButton = view.findViewById(R.id.cancelButton);
+
+        EditText phoneNumber = view.findViewById(R.id.editTextPhone);
+        EditText email = view.findViewById(R.id.editTextTextEmailAddress);
+        EditText userName = view.findViewById(R.id.editTextUsername);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Profile Updated!", Toast.LENGTH_SHORT).show();
-                // Add the update here
+            public void onClick(View view) {
+                boolean isNameValid = isValidUsername(userName, "Username is Required!");
+                boolean isEmailValid = isValidEmail(email);
+                boolean isPhoneNumber = isValidPhoneNumber(phoneNumber);
+
             }
         });
 
-        String defaultUserName = "Name not set";
-        String defaultEmail = "Email not set";
-        String defaultPhone = "Phone number not set";
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                requireActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+    }
 
-        userName.setText(defaultUserName);
-        email.setText(defaultEmail);
-        phoneNumber.setText(defaultPhone);
+    /**
+     * Verifies the required fields have values in them. A toast showing the missing value error.
+     * @param inputText the TextEditInputText value to check
+     * @param e error message that will be shown in the toast
+     * @return True if the inputText is not empty, returns False if is empty
+     */
+    private boolean isValidUsername(EditText inputText, String e){
+        String text = Objects.requireNonNull(inputText.getText()).toString().trim();
+        if (text.isEmpty()) {
+            Toast.makeText(requireContext(), e, Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isValidPhoneNumber(EditText inputText){
+        String text = Objects.requireNonNull(inputText.getText()).toString().trim();
+        if (!inputText.getText().toString().isEmpty())
+        {
+            String phone = PhoneNumberUtils.formatNumber(inputText.getText().toString(), "CA");
+            if (phone == null)
+            {
+                Toast.makeText(requireContext(), "Invalid phone number", Toast.LENGTH_LONG).show();
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isValidEmail(EditText email) {
+        if (email == null || email.getText().toString().trim().isEmpty()) {
+            Toast.makeText(requireContext(), "Enter an email", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        String regexPattern = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        if (!Pattern.compile(regexPattern).matcher(email.getText().toString().trim()).matches()) {
+            Toast.makeText(requireContext(), "Invalid email format", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
     }
 }
