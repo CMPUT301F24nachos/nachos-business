c23d5656f416ce084c3e569ddc0a2ac524c1702e
ruchali1 <ruchali@ualberta.ca>
2024-11-05 10:38:53 -0700

    Profile Images can be browsed

--- app/src/main/java/com/example/nachosbusiness/admin_browse/Profile.java
+++ app/src/main/java/com/example/nachosbusiness/admin_browse/Profile.java
@@ -3,10 +3,12 @@ package com.example.nachosbusiness.admin_browse;
 public class Profile {
     private String name;
     private String image;
+    private String android_id;
 
-    public Profile(String name, String imageUrl) {
+    public Profile(String name, String imageUrl, String android_id) {
         this.name = name;
         this.image = imageUrl;
+        this.android_id = android_id;
     }
 
     public String getName() {
@@ -23,6 +25,14 @@ public class Profile {
 
     public void setImage(String image) {
         this.image = image;
+    }
+
+    public String getAndroid_id() {
+        return android_id;
+    }
+
+    public void setAndroid_id(String android_id) {
+        this.android_id = android_id;
     }
 }
 
