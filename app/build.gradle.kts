a517986809cdc0dc122ac5a9c61c142ebc7b8832
ruchali1 <ruchali@ualberta.ca>
2024-11-06 13:26:22 -0700

    Browse Image Fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-storage")
+    implementation ("com.github.bumptech.glide:glide:4.13.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
