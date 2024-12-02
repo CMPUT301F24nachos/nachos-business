0bdb0386087900be96c1294a3efc180370b0c722
Ruchali Aery <ruchali@ualberta.ca>
2024-12-01 18:24:05 -0700

    autogen images in profile list - also they can't be removed, event image removal toast fix

--- app/src/main/java/com/example/nachosbusiness/admin_browse/ProfileArrayAdapter.java
+++ app/src/main/java/com/example/nachosbusiness/admin_browse/ProfileArrayAdapter.java
@@ -76,7 +76,8 @@ public class ProfileArrayAdapter extends ArrayAdapter<Profile> {
         profileName.setText(profile.getName());
 
         String androidId = profile.getAndroid_id(); // Make sure this method exists to retrieve the ID
-        loadProfileImage(androidId, profileImage);
+        String Name = profile.getName();
+        loadProfileImage(androidId, profileImage, Name);
 
         editProfile.setOnClickListener(v -> {
             // When the edit button is clicked, open the profile fragment
@@ -91,7 +92,7 @@ public class ProfileArrayAdapter extends ArrayAdapter<Profile> {
      * @param androidId   The unique Android ID for each user profile.
      * @param imageView   The ImageView to display the profile image.
      */
-    private void loadProfileImage(String androidId, ImageView imageView) {
+    private void loadProfileImage(String androidId, ImageView imageView, String Name) {
         StorageReference storageRef = FirebaseStorage.getInstance().getReference();
         StorageReference profileImageRef = storageRef.child("profile_images/" + androidId + ".jpg");
 
@@ -104,14 +105,56 @@ public class ProfileArrayAdapter extends ArrayAdapter<Profile> {
                     ((Activity) context).runOnUiThread(() -> imageView.setImageBitmap(bitmap));
                 } catch (IOException e) {
                     e.printStackTrace();
+                    // In case of an error, handle it by showing a default image
+                    showDefaultImage(imageView, null, Name);
                 }
             }).start();
         }).addOnFailureListener(e -> {
             e.printStackTrace();
-            ((Activity) context).runOnUiThread(() -> imageView.setImageResource(R.drawable.profile_picture_drawable));
+            // In case of failure, handle it by showing a default image
+            showDefaultImage(imageView, e, Name);
         });
     }
 
+    private void showDefaultImage(ImageView imageView, Exception e, String Name) {
+        // Check if the username exists and generate the default image based on it
+        // Get the username from the profile (or any other source if needed)
+        String username = Name; // You might need to pass this to the method if it's available
+
+        // Generate a default image based on the first character of the username
+        char firstChar = Character.toLowerCase(username.charAt(0));
+        StorageReference defaultImageRef;
+
+        if ("abcdefghi".indexOf(firstChar) != -1) {
+            defaultImageRef = FirebaseStorage.getInstance().getReference().child("profile_images/abcdefghi.jpg");
+        } else if ("jklmnopqr".indexOf(firstChar) != -1) {
+            defaultImageRef = FirebaseStorage.getInstance().getReference().child("profile_images/jklmnopqr.jpg");
+        } else if ("stuvwxyz".indexOf(firstChar) != -1) {
+            defaultImageRef = FirebaseStorage.getInstance().getReference().child("profile_images/stuvwxyz.jpg");
+        } else {
+            // If the username doesn't match any known range, use a fallback default image
+            defaultImageRef = FirebaseStorage.getInstance().getReference().child("profile_images/default.jpg");
+        }
+
+        defaultImageRef.getDownloadUrl().addOnSuccessListener(defaultUri -> {
+            new Thread(() -> {
+                try {
+                    InputStream inputStream = new java.net.URL(defaultUri.toString()).openStream();
+                    Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+
+                    // Set the default image to the ImageView
+                    ((Activity) context).runOnUiThread(() -> imageView.setImageBitmap(bitmap));
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                    ((Activity) context).runOnUiThread(() -> imageView.setImageResource(R.drawable.profile_picture_drawable));
+                }
+            }).start();
+        }).addOnFailureListener(defaultError -> {
+            defaultError.printStackTrace();
+            // Fallback to a basic default resource image if all else fails
+            ((Activity) context).runOnUiThread(() -> imageView.setImageResource(R.drawable.profile_picture_drawable));
+        });
+    }
     /**
      * Opens the ProfileDetailFragment and with the profile
      * Replaces the current fragment with the ProfileDetailFragment
