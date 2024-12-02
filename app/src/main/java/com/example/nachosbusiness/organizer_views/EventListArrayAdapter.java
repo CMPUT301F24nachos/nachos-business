c4cf7cd60540b12fd688e60f070abe122644e9a2
Muhammad Umer Fiaz <umerfiaz251@gmail.com>
2024-12-02 00:01:39 -0700

    Update EventListArrayAdapter.java

--- app/src/main/java/com/example/nachosbusiness/organizer_views/EventListArrayAdapter.java
+++ app/src/main/java/com/example/nachosbusiness/organizer_views/EventListArrayAdapter.java
@@ -84,6 +84,13 @@ public class EventListArrayAdapter extends ArrayAdapter<Event> {
         ImageButton editEvent = view.findViewById(R.id.edit_icon);
         Button waitlistButton = view.findViewById(R.id.waitlist_button);
         Button mapButton = view.findViewById(R.id.map_button);
+
+        if (event.getHasGeolocation()) {
+            mapButton.setVisibility(View.VISIBLE);
+        } else {
+            mapButton.setVisibility(View.GONE);
+        }
+        
         TextView eventDescription = view.findViewById(R.id.event_description);
         TextView eventDate = view.findViewById(R.id.event_date);
 
